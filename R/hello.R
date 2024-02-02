283b175baa5166ccd0d60bd082dca4e42f7a5867
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2024-02-02 07:01:18 -0500

    Documentation, testing, README (#1)

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -13,6 +13,13 @@
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
 
+#' hello
+#'
+#' @return Nothing, just prints hello
+#' @export
+#'
+#' @examples
+#' hello()
 hello <- function() {
   print("Hello, world!")
 }
