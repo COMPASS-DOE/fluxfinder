fed8963aff5d644a1592ef3f42ddbd89d9ccfd61
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2024-02-05 15:46:38 -0500

    Add support for LI-7820 (#7)

--- R/read_functions.R
+++ R/read_functions.R
@@ -1,4 +1,41 @@
 
+
+#' Internal utility function to read LI-78x0 files
+#'
+#' @param dat_raw Raw data, character vector
+#' @param model Model name, string
+#' @param quiet Be quiet? Logical
+#' @return A data frame with the parsed data.
+wtf_read_LI78x0 <- function(dat_raw, model, quiet) {
+  # Save the machine serial number
+  sn <- trimws(gsub("SN:\t", "", dat_raw[2], fixed = TRUE))
+  # Parse the timezone from the header and use it to make a TIMESTAMP field
+  tz <- trimws(gsub("Timezone:\t", "", dat_raw[5], fixed = TRUE))
+
+  # These files have five header lines, the column names in line 6,
+  # and then the column units in line 7. We only want the names and then data
+  dat_raw <- dat_raw[-c(1:5, 7)]
+  # Irritatingly, the units line can repeat in the file. Remove these instances
+  dat_raw <- dat_raw[grep("DATAU", dat_raw, invert = TRUE)]
+  # Double irritatingly, if there's no remark, the software writes \t\t, not
+  # \t""\t, causing a read error. Replace these instances
+  dat_raw <- gsub("\t\t", "\tnan\t", dat_raw, fixed = TRUE)
+
+  # Read the data, construct TIMESTAMP, add metadata,
+  # and remove unneeded LI-COR DATE and TIME columns
+  dat <- read.table(textConnection(dat_raw), na.strings = "nan", header = TRUE)
+  dat$TIMESTAMP <- lubridate::ymd_hms(paste(dat$DATE, dat$TIME), tz = tz)
+  dat$TZ <- tz
+  dat$SN <- sn
+  dat$MODEL <- model
+  dat$DATE <- dat$TIME <- NULL
+
+  if(!quiet) message("Read ", nrow(dat), " rows of ", sn, " data, ",
+                     min(dat$TIMESTAMP), " to ", max(dat$TIMESTAMP), " ", tz)
+
+  return(dat)
+}
+
 #' Read a LI-7810 data file
 #'
 #' @param file Filename to read, character
@@ -18,30 +55,27 @@ wtf_read_LI7810 <- function(file, quiet = FALSE) {
   model <- trimws(gsub("Model:\t", "", dat_raw[1], fixed = TRUE))
   if(model != "LI-7810") stop("This does not appear to be a LI-7810 file!")
 
-  # Save the machine serial number
-  sn <- trimws(gsub("SN:\t", "", dat_raw[2], fixed = TRUE))
-  # Parse the timezone from the header and use it to make a TIMESTAMP field
-  tz <- trimws(gsub("Timezone:\t", "", dat_raw[5], fixed = TRUE))
+  wtf_read_LI78x0(dat_raw, model, quiet)
+}
 
-  # These files have five header lines, the column names in line 6,
-  # and then the column units in line 7. We only want the names
-  dat_raw <- dat_raw[-c(1:5, 7)]
-  # Irritatingly, the units line can repeat in the file. Remove these instances
-  dat_raw <- dat_raw[grep("DATAU", dat_raw, invert = TRUE)]
-  # Double irritatingly, if there's no remark, the software writes \t\t, not
-  # \t""\t, causing a read error. Replace these instances
-  dat_raw <- gsub("\t\t", "\tnan\t", dat_raw, fixed = TRUE)
+#' Read a LI-7820 data file
+#'
+#' @param file Filename to read, character
+#' @param quiet Be quiet? Logical
+#' @importFrom utils read.table
+#' @importFrom lubridate ymd_hms
+#' @return A data frame with the parsed data.
+#' @export
+#' @examples
+#' f <- system.file("extdata/TG20-01182.data", package = "whattheflux")
+#' dat <- wtf_read_LI7820(f)
+wtf_read_LI7820 <- function(file, quiet = FALSE) {
 
-  # Read the data, construct TIMESTAMP, add metadata,
-  # and remove unneeded LI-COR DATE and TIME columns
-  dat <- read.table(textConnection(dat_raw), na.strings = "nan", header = TRUE)
-  dat$TIMESTAMP <- lubridate::ymd_hms(paste(dat$DATE, dat$TIME), tz = tz)
-  dat$TZ <- tz
-  dat$SN <- sn
-  dat$DATE <- dat$TIME <- NULL
+  dat_raw <- readLines(file)
 
-  if(!quiet) message("Read in ", nrow(dat), " rows of ", sn, " data, ",
-                     min(dat$TIMESTAMP), " to ", max(dat$TIMESTAMP), " ", tz)
+  # Make sure this is a 7810 file
+  model <- trimws(gsub("Model:\t", "", dat_raw[1], fixed = TRUE))
+  if(model != "LI-7820") stop("This does not appear to be a LI-7820 file!")
 
-  return(dat)
+  wtf_read_LI78x0(dat_raw, model, quiet)
 }
--- tests/testthat/test-read_functions.R
+++ tests/testthat/test-read_functions.R
@@ -14,3 +14,12 @@ test_that("wtf_read_LI7810 works", {
   expect_error(wtf_read_LI7810("data/TG10-01087-bad.data", quiet = TRUE),
                regexp = "does not appear")
 })
+
+test_that("wtf_read_LI7820 works", {
+
+  # Good data
+  x <- wtf_read_LI7820("data/TG20-01182-good.data", quiet = TRUE)
+  expect_s3_class(x, "data.frame")
+  expect_true("SN" %in% names(x)) # parsed serial number from header
+
+})
