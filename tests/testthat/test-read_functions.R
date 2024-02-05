3e7c54053ea338885d6e54fe7af21ac1b4d0b7fe
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2024-02-05 06:11:26 -0500

    Clean up DESCRIPTION, hello.R, tests

--- tests/testthat/test-read_functions.R
+++ tests/testthat/test-read_functions.R
@@ -6,6 +6,11 @@ test_that("wtf_read_LI7810 works", {
   expect_true(is.na(x$CO2[1])) # parsed missing values
   expect_true("SN" %in% names(x)) # parsed serial number from header
 
+  suppressMessages({
+    expect_message(wtf_read_LI7810("data/TG10-01087-good.data", quiet = FALSE))
+  })
+
   # Bad data
-  expect_error(wtf_read_LI7810("data/TG10-01087-bad.data", quiet = TRUE), "does not appear")
+  expect_error(wtf_read_LI7810("data/TG10-01087-bad.data", quiet = TRUE),
+               regexp = "does not appear")
 })
