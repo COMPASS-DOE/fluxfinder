182bd76dd32857dcffa48f30ddb880fa49369ed3
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2024-02-17 08:23:49 -0500

    Compute fluxes for grouped data (#23)
    * Add wtf_compute_fluxes, support function, and tests
    * Update vignette for wtf_compute_fluxes

--- tests/testthat/test-models.R
+++ tests/testthat/test-models.R
@@ -10,3 +10,43 @@ test_that("wtf_fit_models works", {
   expect_s3_class(y, "data.frame")
   expect_true(is.na(y$r.squared_poly))
 })
+
+test_that("wtf_normalize_time works", {
+  # numeric input
+  x <- 2:4
+  expect_equal(wtf_normalize_time(x, TRUE), x - x[1])
+  expect_identical(wtf_normalize_time(x, FALSE), x)
+  # POSIXct input
+  x <- seq(ymd_hms("2024-02-16 20:15:00"), by = "1 min", length.out = 3)
+  expect_equal(wtf_normalize_time(x, TRUE), c(0, 60, 120))
+  expect_identical(wtf_normalize_time(x, FALSE), x)
+})
+
+test_that("wtf_compute_fluxes works", {
+  # Make test data
+  plots <- LETTERS[1:2]
+  times <- 1:3
+  normtimes <- wtf_normalize_time(times, TRUE)
+  x <- expand.grid(Plot = plots, time = times, conc = 1)
+  ff <- function(a, b) data.frame(x = 1) # dummy fit function
+
+  # Normalize times
+  out <- wtf_compute_fluxes(x, "Plot", "time", "conc", 1, 1,
+                            fit_function = ff, normalize_time = TRUE)
+  expect_s3_class(out, "data.frame")
+  expect_identical(out$Plot, plots) # one row per plot
+  expect_identical(out$time, rep(mean(times), nrow(out))) # mean of raw times
+
+  # Raw times
+  out <- wtf_compute_fluxes(x, "Plot", "time", "conc", 1, 1,
+                            fit_function = ff, normalize_time = FALSE)
+  expect_identical(out$Plot, plots) # one row per plot
+  expect_identical(out$time, rep(mean(times), nrow(out))) # mean of raw times
+
+  # Passing NULL for the group column should return a single row
+  out <- wtf_compute_fluxes(x, NULL, "time", "conc", 1, 1,
+                            fit_function = ff, normalize_time = TRUE)
+  expect_s3_class(out, "data.frame")
+  expect_identical(nrow(out), 1L) # one row
+  expect_identical(out$time, rep(mean(times), nrow(out))) # mean of raw times
+})
