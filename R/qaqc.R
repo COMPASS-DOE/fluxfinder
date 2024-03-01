a46271a0c2aaaf2ec425da85bc260da024517059
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2024-03-01 18:00:34 -0500

    Open output HTML file

--- R/qaqc.R
+++ R/qaqc.R
@@ -5,16 +5,22 @@
 #'
 #' @param output_file Name of the output file
 #' @param output_dir Name of the output directory; default is current working directory
+#' @param open_output Automatically open the output HTML file?
+#' @importFrom utils browseURL
 #' @return The path of the output file
 #' @export
 #'
 #' @examples
-#' x <- qaqc()
+#' x <- qaqc(open_output = FALSE)
 #' file.remove(x) # clean up
-qaqc <- function(output_file = "qaqc.html", output_dir = getwd()) {
+qaqc <- function(output_file = "qaqc.html",
+                 output_dir = getwd(),
+                 open_output = TRUE) {
   f <- system.file("qaqc.Rmd", package = "whattheflux")
   if(requireNamespace("rmarkdown", quietly = TRUE)) {
-    rmarkdown::render(f, output_file = output_file, output_dir = output_dir)
+    fout <- rmarkdown::render(f, output_file = output_file, output_dir = output_dir)
+    if(open_output) browseURL(paste0('file://', fout))
+    return(fout)
   } else {
     stop("To run this function, please install the rmarkdown package")
   }
