50ac15387227e016f8fffd5b2666d8076da05f60
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2024-03-01 19:52:53 -0500

    Initial pass at whattheflux QA/QC (#35)
    * Initial structure for a QAQC Rmarkdown doc
    
    * Open output HTML file
    
    * Rearrange things
    
    * Pass data to Rmd; first, initial, QA/QC plots
    
    * Update qaqc.Rmd

--- R/models.R
+++ R/models.R
@@ -77,10 +77,7 @@ wtf_fit_models <- function(time, conc, area, volume) {
 #' @param normalize Normalize the values so that first is zero? Logical
 #' @return A numeric vector of normalized values (if \code{normalize_time} is
 #' TRUE) or the original vector if not.
-#' @export
-#' @examples
-#' wtf_normalize_time(2:4) # returns 0:2
-#' wtf_normalize_time(2:4, FALSE) # returns 2:4
+#' @keywords internal
 wtf_normalize_time <- function(time, normalize = TRUE) {
   if(normalize) {
     as.numeric(time) - as.numeric(min(time, na.rm = TRUE))
--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,14 @@
 # zzz.R - miscellany
 
+#' Check if whattheflux.quiet option is TRUE
+#' @return TRUE or FALSE
+#' @keywords internal
+wtf_isquiet <- function() {
+  getOption("whattheflux.quiet", default = FALSE)
+}
+
 wtf_message <- function(...) {
-  if (getOption("whattheflux.quiet", default = FALSE)) {
+  if (wtf_isquiet()) {
     return()
   }
   message(...)
