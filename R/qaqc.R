c559ab4fb4a7de77bf0244bd1ade42edb3ddc352
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2024-03-02 17:24:23 -0500

    Simplify qaqc params and only show worst-performing points

--- R/qaqc.R
+++ R/qaqc.R
@@ -6,25 +6,23 @@
 #' @param flux_data A data frame from \code{\link{wtf_compute_fluxes}} or similar
 #' @param group_column Name of the grouping label column in \code{flux_data},
 #' character; pass NULL to run with no grouping
-#' @param output_file Name of the output file
-#' @param output_dir Name of the output directory; default is current working directory
 #' @param open_output Automatically open the output HTML file?
+#' @param ... Other parameters passed on to \code{\link[rmarkdown]{render}}
 #' @importFrom utils browseURL
 #' @return The path of the output file
 #' @export
 #'
 #' @examples
 #' # Toy data
-#' cars$Plot <- c("A", "B")
+#' cars$Plot <- LETTERS[1:5]
 #' fd <- wtf_compute_fluxes(cars, "Plot", "speed", "dist")
 #' x <- wtf_qaqc(fd, group_column = "Plot")
 #' file.remove(x) # clean up
 #' # See the introductory vignette for a fully-worked example with real data
 wtf_qaqc <- function(flux_data,
                      group_column,
-                     output_file = "qaqc.html",
-                     output_dir = getwd(),
-                     open_output = TRUE) {
+                     open_output = TRUE,
+                     ...) {
   if(!requireNamespace("rmarkdown", quietly = TRUE)) {
     stop("To run this function, please install the rmarkdown package")
   }
@@ -38,11 +36,10 @@ wtf_qaqc <- function(flux_data,
 
   # Render
   fout <- rmarkdown::render(f,
-                            output_file = output_file,
-                            output_dir = output_dir,
                             quiet = wtf_isquiet(),
                             params = list(flux_data = tf_flux_data,
-                                          group_column = group_column))
+                                          group_column = group_column),
+                            ...)
   if(open_output) browseURL(paste0('file://', fout))
   invisible(fout)
 }
