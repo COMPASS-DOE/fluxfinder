5ae100aade904be19fdfe8e7e3e29a43a76f350f
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2024-03-02 22:20:13 -0500

    Add flux and r2 columns user-specifiable

--- R/qaqc.R
+++ R/qaqc.R
@@ -6,6 +6,10 @@
 #' @param flux_data A data frame from \code{\link{wtf_compute_fluxes}} or similar
 #' @param group_column Name of the grouping label column in \code{flux_data},
 #' character; pass NULL to run with no grouping
+#' @param flux_column Name of the column with flux estimates; defaults to
+#' "slope_estimate", the output of \code{\link{wtf_compute_fluxes}}
+#' @param r2_column Name of the column with adjusted R2 values; defaults to
+#' "adj.r.squared", the output of \code{\link{wtf_compute_fluxes}}
 #' @param open_output Automatically open the output HTML file?
 #' @param ... Other parameters passed on to \code{\link[rmarkdown]{render}}
 #' @importFrom utils browseURL
@@ -21,6 +25,8 @@
 #' # See the introductory vignette for a fully-worked example with real data
 wtf_qaqc <- function(flux_data,
                      group_column,
+                     flux_column = "slope_estimate",
+                     r2_column = "adj.r.squared",
                      open_output = TRUE,
                      ...) {
   if(!requireNamespace("rmarkdown", quietly = TRUE)) {
@@ -38,7 +44,9 @@ wtf_qaqc <- function(flux_data,
   fout <- rmarkdown::render(f,
                             quiet = wtf_isquiet(),
                             params = list(flux_data = tf_flux_data,
-                                          group_column = group_column),
+                                          group_column = group_column,
+                                          flux_column = flux_column,
+                                          r2_column = r2_column),
                             ...)
   if(open_output) browseURL(paste0('file://', fout))
   invisible(fout)
