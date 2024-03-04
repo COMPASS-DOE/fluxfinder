e7b14d81a63c54a98d899ba1024316c414a18475
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2024-03-04 03:11:20 -0500

    Add support for EGM-4 (#38)

--- tests/testthat/test-read_functions.R
+++ tests/testthat/test-read_functions.R
@@ -35,7 +35,7 @@ test_that("wtf_read_LGR works", {
   expect_true("SN" %in% names(x)) # parsed serial number from header
   # Respects time zone setting
   x <- wtf_read_LGR915("data/LGR-good-data.csv", tz = "EST")
-  expect_identical(tz(x$Time[1]), "EST")
+  expect_true(all(tz(x$Time) == "EST"))
 })
 
 test_that("wtf_read_PicarroG2301 works", {
@@ -48,5 +48,25 @@ test_that("wtf_read_PicarroG2301 works", {
   expect_true("MODEL" %in% names(x))
   # Respects time zone setting
   x <- wtf_read_PicarroG2301("data/PicarroG2301-good-data.dat", tz = "EST")
+  expect_true(all(tz(x$TIMESTAMP) == "EST"))
+})
+
+test_that("wtf_read_EGM4 works", {
+
+  withr::local_options(list(whattheflux.quiet = TRUE))
+
+  # Good data
+  x <- wtf_read_EGM4("data/EGM4-good-data.dat", 2023)
+  expect_s3_class(x, "data.frame")
+  expect_true("MODEL" %in% names(x))
+  expect_true("SOFTWARE" %in% names(x))
+  expect_true(all(lubridate::year(x$TIMESTAMP) == 2023))
+
+  # Respects time zone setting
+  x <- wtf_read_EGM4("data/EGM4-good-data.dat", 2023, tz = "EST")
   expect_identical(tz(x$TIMESTAMP[1]), "EST")
+
+  # Bad data
+  expect_error(wtf_read_EGM4("data/EGM4-bad-data.dat"),
+               regexp = "does not look like")
 })
