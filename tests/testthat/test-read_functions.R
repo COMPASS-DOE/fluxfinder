b6648b7fd47032204bac524c7ffb75c67791e6a0
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2024-03-04 02:15:35 -0500

    Add support for Picarro G2301 (#37)

--- tests/testthat/test-read_functions.R
+++ tests/testthat/test-read_functions.R
@@ -31,8 +31,22 @@ test_that("wtf_read_LGR works", {
   # Good data
   x <- wtf_read_LGR915("data/LGR-good-data.csv")
   expect_s3_class(x, "data.frame")
+  expect_true("MODEL" %in% names(x))
   expect_true("SN" %in% names(x)) # parsed serial number from header
   # Respects time zone setting
   x <- wtf_read_LGR915("data/LGR-good-data.csv", tz = "EST")
   expect_identical(tz(x$Time[1]), "EST")
+})
+
+test_that("wtf_read_PicarroG2301 works", {
+
+  withr::local_options(list(whattheflux.quiet = TRUE))
+
+  # Good data
+  x <- wtf_read_PicarroG2301("data/PicarroG2301-good-data.dat")
+  expect_s3_class(x, "data.frame")
+  expect_true("MODEL" %in% names(x))
+  # Respects time zone setting
+  x <- wtf_read_PicarroG2301("data/PicarroG2301-good-data.dat", tz = "EST")
+  expect_identical(tz(x$TIMESTAMP[1]), "EST")
 })
