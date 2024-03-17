d365a5f03e6b0570d26c63394dabc46c278d0488
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2024-03-17 12:34:27 -0400

    Add second function for clarity of concentration-to-quantity conversion

--- R/models.R
+++ R/models.R
@@ -225,20 +225,28 @@ wtf_compute_fluxes <- function(data,
 }
 
 
-#' Convert ppm to micromoles using the Ideal Gas Law
+
+#' Convert gas concentration to quantity using the Ideal Gas Law
 #'
-#' @param ppm Gas concentration (ppmv), numeric
 #' @param volume System volume (chamber + tubing + analyzer, m3), numeric
 #' @param temp Optional chamber temperature (degrees C), numeric
 #' @param atm Optional atmospheric pressure (Pa), numeric
-#' @return The value(s) in micromoles.
-#' @export
+#' @return The quantity value, in micromoles (for \code{wtf_ppm_to_umol})
+#' or nanomoles (for \code{wtf_ppb_to_nmol}).
 #' @references Steduto et al.:
 #' Automated closed-system canopy-chamber for continuous field-crop monitoring
 #' of CO2 and H2O fluxes, Agric. For. Meteorol., 111:171-186, 2002.
 #' \url{http://dx.doi.org/10.1016/S0168-1923(02)00023-0}
 #' @note If \code{temp} and/or \code{atm} are not provided, the defaults
 #' are NIST normal temperature and pressure.
+#' @name ideal-gas-law
+NULL
+
+#' @rdname ideal-gas-law
+#' @param ppm Gas concentration, numeric
+#' @examples
+#' wtf_ppm_to_umol(400, 0.1)
+#' @export
 wtf_ppm_to_umol <- function(ppm, volume, temp, atm) {
 
   if(missing(temp)) {
@@ -257,4 +265,13 @@ wtf_ppm_to_umol <- function(ppm, volume, temp, atm) {
 
   # Use ideal gas law to calculate micromoles: n = pV/RT
   return(ppm * atm * volume / (R * TEMP_K))
+}
+
+#' @rdname ideal-gas-law
+#' @param ppb Gas concentration (ppbv), numeric
+#' @examples
+#' wtf_ppb_to_nmol(400, 0.1)
+#' @export
+wtf_ppb_to_nmol <- function(ppb, volume, temp, atm) {
+  wtf_ppm_to_umol(ppb, volume, temp, atm)
 }
